4cbfc583a67c4fb963b70232c189375fc6434882
davi fontana <fontana.davi2009@gmail.com>
2023-12-21 10:51:33 -0300

    Javascript

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,3 +3,25 @@ alert('helo world')
 teste = 'Valor modificado com sucesso'
 
 console.log(teste)
+
+if (8 === 8) {
+    document.write('Verdadeiro')
+} else {
+    document.write('falso')
+}
+
+var name = 'Verdadeiro'
+
+if (true) {
+    document.write('...' + name + '...')
+}
+var variavel1 = prompt('Digite um número')
+var variavel2 = prompt('Digite outro número')
+
+variavel1 = parseInt(variavel1)
+variavel2 = parseInt(variavel2)
+
+document.write(variavel1 + variavel2)
+
+
+
