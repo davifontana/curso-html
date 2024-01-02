6671bce14351d2150ae632db8ae0f3f07a5158f1
davi fontana <fontana.davi2009@gmail.com>
2024-01-02 10:11:57 -0300

    JS

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,27 +1,56 @@
-alert('helo world')
+//alert('helo world')
 
-teste = 'Valor modificado com sucesso'
+//teste = 'Valor modificado com sucesso'
 
-console.log(teste)
+//console.log(teste)
 
-if (8 === 8) {
-    document.write('Verdadeiro')
-} else {
-    document.write('falso')
+//if (8 === 8) {
+//document.write('Verdadeiro')
+//} else {
+//document.write('falso')
+//}
+
+//var name = 'Verdadeiro'
+
+//if (true) {
+//document.write('...' + name + '...')
+//}
+//var variavel1 = prompt('Digite um número')
+//var variavel2 = prompt('Digite outro número')
+
+//variavel1 = parseInt(variavel1)
+//variavel2 = parseInt(variavel2)
+
+//document.write(variavel1 + variavel2)
+
+//var joao = parseInt(prompt('Digite a Idade de João'))
+//var maria = parseInt(prompt('Digite a Idade de Maria'))
+
+//if (joao > maria) {
+//document.write('João é mais velho que maria')
+//}
+
+//else if (joao === maria) {
+//document.write('Ambos tem a mesma idade')
+//}
+
+//else {
+//document.write('Maria é mais velha que joão')
+//}
+
+/*function calcarea(width, height) {
+
+var area = width * height
 }
+var width = prompt('Gimme Numba')
+var height = prompt('Gimme othe Numba')
+var area = calcarea(width, height)
 
-var name = 'Verdadeiro'
-
-if (true) {
-    document.write('...' + name + '...')
+document.write(area)
+return area
+       
+/*function sum(a, b) {
+return a + b
 }
-var variavel1 = prompt('Digite um número')
-var variavel2 = prompt('Digite outro número')
-
-variavel1 = parseInt(variavel1)
-variavel2 = parseInt(variavel2)
-
-document.write(variavel1 + variavel2)
-
-
-
+    
+console.log(sum(7, 7))*/
